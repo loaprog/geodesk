138ad2df4defb72d7cedfae7dab3707068b5334e
Leonardo Alves <loa_ava@hotmail.com>
2026-08-31 13:07:58 -0300

    feat: remover configuração de cache de declarações do banco de dados

--- src/database/database.py
+++ src/database/database.py
@@ -11,9 +11,6 @@ from src.configs.settings import settings
 engine = create_async_engine(
     settings.DATABASE_URL,
     pool_pre_ping=True,
-    connect_args={
-        "statement_cache_size": 0,
-    },
 )
 
 
